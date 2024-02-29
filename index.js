179ad1961e440bc683caa8e0b62708c1773b38df
SatyajitPanaskar <satyajitpanaskarsp@gmail.com>
2024-02-29 18:52:51 +0530

    push
    push

---
+++ index.js
@@ -0,0 +1,10 @@
+// const {
+//   findActorByMoviesName,
+//   findMoviesByActorName,
+// } = require("./actorActress");
+
+// // Search Actors by movie name
+// console.log(findActorByMoviesName("Koi Mil Gaya"));
+
+// // search movie by actor name
+// console.log(findMoviesByActorName("Akshay Kumar"));
